b215c9be96af4a86b9c730265c9233e7b6b01042
StoreBale <71181894+StoreBale@users.noreply.github.com>
2024-09-01 01:39:35 +0800

    Update first.py
    connect success

--- first.py
+++ first.py
@@ -1,14 +1,13 @@
 # import os
 # os.environ['TDSDUMP'] = 'stdout'
 
-import pymssql
+import pymysql
 
-conn = pymssql.connect(
-    server='Mysql@localhost:3306',
+conn = pymysql.connect(
+    host='localhost',
     user='root',
     password='password',
     database='my_db',
-    as_dict=True
 )
 
 try:
@@ -19,5 +18,32 @@ try:
 except Exception as e:
     print("Error connecting to database:",e)
 
-    
+
+# import mysql.connector
+
+# # 连接到MySQL数据库
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="password",
+#   database="my_db",
+#   auth_plugin='mysql_native_password'
+# )
+
+# # 创建一个游标对象
+# mycursor = mydb.cursor()
+
+# # 执行SQL查询
+# mycursor.execute("SELECT * FROM users")
+
+# # 获取查询结果
+# result = mycursor.fetchall()
+
+# # 打印查询结果
+# for row in result:
+#   print(row)
+
+# # 关闭游标和数据库连接
+# mycursor.close()
+# mydb.close()
 # print("test")
